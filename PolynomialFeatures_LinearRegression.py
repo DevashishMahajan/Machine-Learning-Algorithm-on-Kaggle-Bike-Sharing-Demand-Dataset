0aa75111ff71e8427d5b42a624dba239a997316c
Devashish <32193655+DevashishMahajan@users.noreply.github.com>
2022-07-07 11:22:52 +0530

    submission API added
    Adding submission file format and kaggle API for submitting predictions. After submission, kaggle provides us public score and ranking based on accuracy of predictions on test dataset

--- PolynomialFeatures_LinearRegression.py
+++ PolynomialFeatures_LinearRegression.py
@@ -4,13 +4,14 @@ Created on Fri May 13 15:22:51 2022
 
 @author: Devashish
 """
-
+# Import necessary libraries 
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-
+# Read csv files from kaggle dataset as Pandas Dataframe
+# Change the file path as the file path in your computer
 train = pd.read_csv(r"C:\Users\Devashish\Downloads\train.csv",parse_dates=['datetime'])
 train['year'] = train['datetime'].dt.year
 train['month'] = train['datetime'].dt.month
@@ -25,8 +26,10 @@ test['day'] = test['datetime'].dt.day
 test['hour'] = test['datetime'].dt.hour
 test['weekday'] = test['datetime'].dt.weekday
 
-
+# X is a feature 
 x= train.drop(['datetime','casual', 'registered', 'count'],axis=1)
+
+#y is a label
 y_c= train['casual']
 y_p= train['registered']
 X_test=test.drop(['datetime'],axis=1)
@@ -38,6 +41,7 @@ poly_test = poly.transform(X_test)
 le_c = LinearRegression()
 le_c.fit(poly_feat,y_c)
 
+# Making predictions on test set
 y_pred = np.round(le_c.predict(poly_test))
 print(y_pred.min())
 y_pred[y_pred<0]=0
@@ -50,5 +54,15 @@ print(y_pred_p.min())
 y_pred_p[y_pred_p<0]=0
 
 y_count=y_pred+y_pred_p
+
+# Cretiing Dataframe as per the required format of kaggle competition
 submit = pd.DataFrame({'datetime':test.datetime,'count':y_count})
-submit.to_csv("submit_13_05.csv",index=False)
+# Converting Dataframe into CSV file
+submit.to_csv("submit_kaggle_Bike Sharing Demand.csv",index=False)
+
+# Upload above file "submit_kaggle_Bike Sharing Demand.csv" at "https://www.kaggle.com/competitions/bike-sharing-demand/submit"
+# OR
+# Use kaggle API "kaggle competitions submit -c bike-sharing-demand -f submit_kaggle_Bike Sharing Demand.csv -m "Message_PolynomialFeatures_LinearRegression""
+
+
+
