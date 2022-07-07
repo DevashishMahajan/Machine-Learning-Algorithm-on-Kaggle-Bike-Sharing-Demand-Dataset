405c5d5298db2814a68fe176979e73376dd4d6a5
Devashish <32193655+DevashishMahajan@users.noreply.github.com>
2022-07-07 11:12:38 +0530

    PolynomialFeatures_LinearRegression.py for kaggle bike sharing demand competition
    Polynomial Features(degree=2) for Linear Regression for making predictions on kaggle bike sharing demand competition. Along with .csv file to submit.

---
+++ PolynomialFeatures_LinearRegression.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 15:22:51 2022
+
+@author: Devashish
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+train = pd.read_csv(r"C:\Users\Devashish\Downloads\train.csv",parse_dates=['datetime'])
+train['year'] = train['datetime'].dt.year
+train['month'] = train['datetime'].dt.month
+train['day'] = train['datetime'].dt.day
+train['hour'] = train['datetime'].dt.hour
+train['weekday'] = train['datetime'].dt.weekday
+
+test = pd.read_csv(r"C:\Users\Devashish\Downloads\test.csv",parse_dates=['datetime'])
+test['year'] = test['datetime'].dt.year
+test['month'] = test['datetime'].dt.month
+test['day'] = test['datetime'].dt.day
+test['hour'] = test['datetime'].dt.hour
+test['weekday'] = test['datetime'].dt.weekday
+
+
+x= train.drop(['datetime','casual', 'registered', 'count'],axis=1)
+y_c= train['casual']
+y_p= train['registered']
+X_test=test.drop(['datetime'],axis=1)
+
+poly = PolynomialFeatures(degree=2)
+poly_feat = poly.fit_transform(x)
+poly_test = poly.transform(X_test)
+
+le_c = LinearRegression()
+le_c.fit(poly_feat,y_c)
+
+y_pred = np.round(le_c.predict(poly_test))
+print(y_pred.min())
+y_pred[y_pred<0]=0
+
+le_p = LinearRegression()
+le_p.fit(poly_feat,y_p)
+
+y_pred_p = np.round(le_p.predict(poly_test))
+print(y_pred_p.min())
+y_pred_p[y_pred_p<0]=0
+
+y_count=y_pred+y_pred_p
+submit = pd.DataFrame({'datetime':test.datetime,'count':y_count})
+submit.to_csv("submit_13_05.csv",index=False)
