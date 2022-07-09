17ea2761b0499ea9937511a826973a2234736482
Devashish <32193655+DevashishMahajan@users.noreply.github.com>
2022-07-09 12:41:11 +0530

    Code for submission file added
    Code for submission of .csv file added in the program. File can be submitted to kaggle competition page.

--- DecisionTreeRegressor_Kaggle_BikeSharingDemand.py
+++ DecisionTreeRegressor_Kaggle_BikeSharingDemand.py
@@ -4,7 +4,7 @@ Created on Wed May 18 17:32:45 2022
 
 @author: Devashish
 """
-
+# Import necessary libraries 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -12,10 +12,13 @@ from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
 from sklearn.model_selection import GridSearchCV, StratifiedKFold,KFold
 from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
+
+# Change the file path as the file path in your computer
 import os
 os.chdir("D:\CDAC ML\Cases\Kaggle\Tabular Playground Series - Jan 2022")
 
 
+# Read csv files from kaggle dataset as Pandas Dataframe
 
 train = pd.read_csv(r"train.csv",parse_dates=['date'],index_col='row_id')
 train['year'] = train['date'].dt.year
@@ -33,7 +36,11 @@ test['weekday'] = test['date'].dt.weekday
 
 
 train1= train.drop(['date','num_sold'],axis=1)
+
+# X is a feature 
 X=pd.get_dummies(train1,drop_first=True)
+
+#y is a label
 y= train['num_sold']
 
 X_test=test.drop(['date'],axis=1)
@@ -52,12 +59,16 @@ print(gcv1.best_score_)
 print(gcv1.best_estimator_)
 
 best_model1=gcv1.best_estimator_
+
+# Making predictions on test set
 y_pred1= np.round(best_model1.predict(X_test))
 y_pred1[y_pred1<0]=0
 
 
-
+# Cretiing Dataframe as per the required format of kaggle competition
 submit = pd.DataFrame({'date':test.date,'count':y_pred1})
+
+# Converting Dataframe into CSV file
 submit.to_csv("Tabular Playground Series - Jan 2022_submit_18_05_3nd.csv",index=False)
 
 
@@ -73,11 +84,20 @@ print(gcv2.best_score_)
 
 
 best_model2=gcv2.best_estimator_
+
+# Making predictions on test set
 y_pred2= (np.round(best_model2.predict(X_test))+1)
 y_pred2[y_pred2<0]=0
 
 
 
 y_count=y_pred1+y_pred2
+
+# Cretiing Dataframe as per the required format of kaggle competition
 submit = pd.DataFrame({'date':test.date,'count':y_count})
+
+# Converting Dataframe into CSV file
 submit.to_csv("Tabular Playground Series - Jan 2022_submit_18_05_3nd.csv",index=False)
+
+
+
